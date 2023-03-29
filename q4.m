d264caaf45caf6a3af9656e27bdc185dc40a4547
Waterdrinker88 <48976062+Waterdrinker88@users.noreply.github.com>
2023-03-29 15:54:24 -0500

    finished q4, calculates p02

--- q4.m
+++ q4.m
@@ -2,4 +2,14 @@
 %COE 347 Introduction to Computational Fluid Dynamics
 %OF3 Q4
 clc;clear;close all;
-%Fuck i hate git
+
+p1 = 1; %atm
+gamma = 1.4;
+M1 = 3; 
+
+term1 = (gamma-1)/2; 
+term2 = gamma/(gamma-1);
+p01 = p1*(1+term1*M1^2)^2
+
+p02p01 = 0.32834388; 
+p02 = p02p01*p01
